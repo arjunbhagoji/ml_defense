c25d76a734544113e1fa89e52067d86443136746
Chawin Sitawarin <chawins@princeton.edu>
2017-04-12 02:08:56 -0400

    svm works with l1 norm

--- lib/utils/dr_utils.py
+++ lib/utils/dr_utils.py
@@ -5,9 +5,18 @@ from lib.utils.data_utils import *
 
 #------------------------------------------------------------------------------#
 def pca_dr(X_train, X_test, rd, X_val=None, rev=None):
-    #Fitting the PCA model on training data
+
+    """
+    Perform PCA on X_train then transform X_train, X_test (and X_val).
+    Return transformed data in original space if rev is True; otherwise, return
+    transformed data in PCA space.
+    """
+
+    # Fit PCA model on training data, random_state is specified to make sure
+    # result is reproducible
     pca = PCA(n_components=rd, random_state=10)
     pca.fit(X_train)
+
     # Transforming training and test data
     X_train_dr = pca.fit_transform(X_train)
     X_test_dr = pca.transform(X_test)
@@ -27,6 +36,13 @@ def pca_dr(X_train, X_test, rd, X_val=None, rev=None):
 
 #------------------------------------------------------------------------------#
 def random_proj_dr(X_train, X_test, rd, rev=None):
+
+    """
+    Perform Gaussian Random Projection on X_train then transform X_train, X_test
+    (and X_val). Return transformed data in original space if rev is True;
+    otherwise, return transformed data in PCA space.
+    """
+
     # Generating random matrix for projection
     grp = GRP(n_components=rd, random_state=10)
     X_train_dr = grp.fit_transform(PCA_in_train)
@@ -40,6 +56,11 @@ def random_proj_dr(X_train, X_test, rd, rev=None):
 
 #------------------------------------------------------------------------------#
 def dr_wrapper(X_train, X_test, DR, rd, X_val=None, rev=None):
+
+    """
+    A wrapper function for dimensionality reduction functions.
+    """
+
     data_dict = get_data_shape(X_train, X_test, X_val)
     no_of_dim = data_dict['no_of_dim']
 
@@ -48,35 +69,38 @@ def dr_wrapper(X_train, X_test, DR, rd, X_val=None, rev=None):
     no_of_features = data_dict['no_of_features']
 
     # Reshape for PCA function
-    DR_in_train = X_train.reshape(train_len,no_of_features)
-    DR_in_test = X_test.reshape(test_len,no_of_features)
+    DR_in_train = X_train.reshape(train_len, no_of_features)
+    DR_in_test = X_test.reshape(test_len, no_of_features)
     if X_val is not None:
         val_len = data_dict['val_len']
-        DR_in_val = X_val.reshape(val_len,no_of_features)
-    else: DR_in_val = None
+        DR_in_val = X_val.reshape(val_len, no_of_features)
+    else:
+        DR_in_val = None
 
+    #----------------------------------- PCA ----------------------------------#
     if DR == 'pca':
         if X_val is not None:
             X_train, X_test, X_val, dr_alg = pca_dr(DR_in_train, DR_in_test, rd,
-                                                                DR_in_val, rev)
+                                                    DR_in_val, rev)
         else:
             X_train, X_test, dr_alg = pca_dr(DR_in_train, DR_in_test, rd,
-                                                                DR_in_val, rev)
+                                             DR_in_val, rev)
+    #----------------------------- Random Projection --------------------------#
     elif DR == 'rp':
         if X_val is not None:
             X_train, X_test, X_val, dr_alg = pca_dr(DR_in_train, DR_in_test, rd,
-                                                                DR_in_val, rev)
+                                                    DR_in_val, rev)
         else:
             X_train, X_test, dr_alg = pca_dr(DR_in_train, DR_in_test, rd,
-                                                                DR_in_val, rev)
+                                             DR_in_val, rev)
 
-    if no_of_dim == 3 or (no_of_dim==4 and rev == None):
+    if (no_of_dim == 3) or ((no_of_dim == 4) and (rev == None)):
         channels = data_dict['channels']
         X_train = X_train.reshape((train_len, channels, rd))
         X_test = X_test.reshape((test_len, channels, rd))
         if X_val is not None:
             X_val = X_val.reshape((val_len, channels, rd))
-    elif (no_of_dim == 4 and rev != None):
+    elif (no_of_dim == 4) and (rev != None):
         channels = data_dict['channels']
         height = data_dict['height']
         width = data_dict['width']
